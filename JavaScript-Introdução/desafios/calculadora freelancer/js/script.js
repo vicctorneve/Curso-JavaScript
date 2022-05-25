c99148e8401b69d5f0c3a3d0cb715e4ac51a7796
vicctorneves <vicctorneves88@gmail.com>
2022-05-25 14:15:53 -0300

    html + css pronto

--- JavaScript-Introdução/desafios/calculadora freelancer/js/script.js
+++ JavaScript-Introdução/desafios/calculadora freelancer/js/script.js
@@ -1,25 +1,41 @@
 // SCRIPT CALCULADORA FREELANCER
+
 let horaJob = 8;
-let taxaDiaria;
 let taxaHora = document.querySelector('#taxahora');
-let orcamento = document.querySelector('#orcamento');
-let diasUteis;
+let orcamento = document.querySelector("#orcamento");
+let result = document.querySelector("#container-result");
+const descontoMostrar = '40%';
+const descontoConta = 0.40;
+let valorMensal; 
+let valorTotal = valorMensal 
 
-let result = document.querySelector('#texto');
-
-send.addEventListener( calcular);
-
-function calcular(){
-   // Taxa por hora trbalhada
+function calcular() {
    taxaHora = taxaHora.value;
-   result.innerHTML += `<p> Taxa por hora trabalhada: ${taxaHora} </p>`;
-
-   //taxa Diaria
-   taxaDiaria = taxaHora * horaJob;
-   result.innerHTML += `<p> Taxa diaria: ${taxaDiaria} </p>`;
-
    orcamento = orcamento.value;
-   diasUteis = orcamento / taxaDiaria;
-   result.innerHTML += `<p> Dias para realizar o projeto: ${Math.round(diasUteis) } </p>`;
 
+   //Taxa Horaria
+   result.innerHTML += `<p> Taxa por hora trabalhada: R$: ${taxaHora} </p>`;
+   // Taxa Diara
+   let taxaDiaria = taxaHora * 8;
+   result.innerHTML += `<p> Taxa Diaria: R$: ${taxaDiaria} </p>`;
+
+   // condição - se o checkbox for checked, faça
+  
+   //Orcamento
+   result.innerHTML += `<p> Orçamento: R$: ${orcamento} </p>`;
+
+   const diasUteis = orcamento / taxaDiaria
+   result.innerHTML += `<p> Dias uteis: ${Math.round(diasUteis)} </p>`;
+
+   //Desconto
+   result.innerHTML += `<p> Desconto: ${descontoMostrar} </p>`;
+
+   result.innerHTML += `<p> Valor Mensal: ${valorMensal} </p>`;
+
+   //Fim condição
+
+   result.innerHTML += `<p> Valor Total: ${valorTotal} </p>`;
+}
+function reset() {
+   result.innerHTML = 'oi';
 }
