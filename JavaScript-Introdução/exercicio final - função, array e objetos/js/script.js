7e2664cd6569cb83fdf772adabd9e46c9841a888
vicctorneves <vicctorneves88@gmail.com>
2022-06-04 15:57:17 -0300

    exercicio final - introdução

---
+++ JavaScript-Introdução/exercicio final - função, array e objetos/js/script.js
@@ -0,0 +1,34 @@
+const form = document.querySelector('.form');
+const resultado = document.querySelector('.resultado');
+
+
+const pessoas = [];
+function recebeEventoForm (evento) {
+   evento.preventDefault();
+
+   const nome = form.querySelector('#nome');
+   const sobrenome = form.querySelector('#sobrenome');
+   const peso = form.querySelector('#peso');
+   const altura = form.querySelector('#altura');
+
+   const imc = peso.value/ (altura.value**2);
+
+   //adiciona no array e cria object 
+   pessoas.push({
+      nome:nome.value,
+      sobrenome: sobrenome.value,
+      peso: peso.value,
+      altura: altura.value,
+      imc: imc
+   });
+
+   console.log(pessoas);
+   resultado.innerHTML += `<p> ${nome.value} ${sobrenome.value} pesa ${peso.value} e sua altura é ${altura.value} e tem IMC de ${imc.toFixed(2)}</p>`
+}
+form.addEventListener('submit', recebeEventoForm);
+
+const btnReset = document.querySelector('#btn-reset');
+
+btnReset.addEventListener('click', function(){
+   resultado.innerHTML ='';
+})
