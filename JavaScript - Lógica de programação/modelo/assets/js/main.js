0bcc4705851d52229fe977a2d84594916b75da65
vicctorneves <vicctorneves88@gmail.com>
2022-06-19 10:50:48 -0300

    exercicio imc

--- JavaScript - Lógica de programação/modelo/assets/js/main.js
+++ JavaScript - Lógica de programação/modelo/assets/js/main.js
@@ -1,71 +1,53 @@
-const form = document.querySelector('form')
-form.addEventListener('submit', contentForm)
-const result = document.querySelector('.result')
-const classification = document.querySelectorAll('.classification p')
-const imcUsuario = document.querySelector('.imc')
+const form = document.querySelector('#form');
+form.addEventListener('submit', function (e){
+   e.preventDefault();
+   const inputPeso = e.target.querySelector('#peso');
+   const inputAltura = e.target.querySelector('#altura');  
+   
+   const peso = Number(inputPeso.value);
+   const altura = Number(inputAltura.value);
 
-function contentForm(evento){
-   evento.preventDefault();
-   const weight = document.querySelector('#weight');
-   const height = document.querySelector('#height');
-   let tipo;
-   const imc = weight.value / (height.value**2);
-   console.log(imc.toFixed(2));
-
-
-   function removeColor(){
-      for(res of classification){
-         res.style.backgroundColor = 'white';
-         res.style.fontWeight = 'normal';
-      }
+   if (!peso){
+      setResultado('Peso Invalido', false);
+      return;
    }
-   function executar(){
-      removeColor();
-      showImc();
+   if (!altura){
+      setResultado('Altura invalida', false);
+      return;
    }
-   function showImc(){
-      
-      imcUsuario.innerHTML = imc.toFixed(2);
-   }
-
-   if (imc < 18.5){
-      tipo = 'Abaixo do peso';
-      executar()
-      classification[0].style.fontWeight= 'bold';
-      classification[0].style.backgroundColor= 'aqua';
-   } else if (imc >=18.5 && imc <= 24.9) {
-      tipo = 'Peso Normal';
-      executar()
-      classification[1].style.fontWeight= 'bold';
-      classification[1].style.backgroundColor= 'green';
-   } else if (imc >=25 && imc <= 29.9) {
-      tipo = 'Sobrepeso';
-      executar()
-      classification[2].style.fontWeight= 'bold';
-      classification[2].style.backgroundColor= 'yellow';
-   } else if (imc >=30 && imc <= 34.9) {
-      tipo = 'Obesidade grau 1';
-      executar()
-      classification[3].style.fontWeight= 'bold';
-      classification[3].style.backgroundColor= 'orange';
-   } else if (imc >=35 && imc <= 39.9) {
-      tipo = 'Obesidade grau 2';
-      executar()
-      classification[4].style.fontWeight= 'bold';
-      classification[4].style.backgroundColor= 'orangered';
-   } else if (imc >=40) {
-      executar()
-      classification[5].style.fontWeight= 'bold';
-      classification[5].style.backgroundColor= 'red';
-      tipo = 'Obesidade grau 3';
-   }
-   if (weight.value === ''){
-      result.innerHTML = 'Peso Invalido!!';
-      result.style.backgroundColor = 'red';
-   } else if (height.value === ''){
-      result.innerHTML = 'Altura Invalida!';
-      result.style.backgroundColor = 'red';
-   }
+   const imc = calcImc(peso, altura);
+   const nivelImc = getNivelImc(imc); 
+   const msg = `${imc} (${nivelImc})`
+   setResultado(msg, true)
+})
+function calcImc(peso, altura){
+   const imc = peso / (altura**2);
+   return imc.toFixed(2)
 }
+function getNivelImc(imc){
+   const nivel = ['Abaixo do peso', 'Peso Normal', 'Sobrepeso', 'Obesidade Grau 1', 'Obesidade Grau 2', 'Obesidade Grau 4'];
 
+   if (imc >= 39.9) return nivel[5]
+   if (imc >= 34.9) return nivel[4]
+   if (imc >= 29.9) return nivel[3]
+   if (imc >= 24.9) return nivel[2]
+   if (imc >= 18.5) return nivel[1]
+   if (imc < 18.5) return nivel[0]
+}
+function criaP() {
+   const p = document.createElement('p');
+   return p;
+}
+function setResultado(msg, isValid){
+   const resultado = document.querySelector('#resultado');
+   resultado.innerHTML = '';
+   const p = criaP();
 
+   if (isValid) {
+      p.classList.add('color-valido')
+   } else {
+      p.classList.add('color-invalido')
+   }
+   p.innerHTML = msg;
+   resultado.appendChild(p);
+}
