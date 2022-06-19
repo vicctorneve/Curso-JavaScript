586e2e5d805faf1266a862e07b20e612e78aee06
vicctorneves <vicctorneves88@gmail.com>
2022-06-19 15:17:26 -0300

    operação ternária

---
+++ JavaScript - Lógica de programação/aula 5 - Operação ternária/js/script.js
@@ -0,0 +1,6 @@
+
+// (Condição) ? (Valor para verdadeiro) : (valor para falso);
+const pontuaçãoUsuario = 5000;
+const nivelUsuario = pontuaçãoUsuario >= 1000 ? 'Usuário Vip' : 'Usuário Normal';
+
+console.log(nivelUsuario);
