12b029c392d66c57d88a7bbe79f0d7a7df987cb7
vicctorneves <vicctorneves88@gmail.com>
2022-06-20 20:24:27 -0300

    function Switch/case

---
+++ JavaScript - Lógica de programação/aula 7 - Switvh/Case/js/script.js
@@ -0,0 +1,70 @@
+
+
+
+// diaSemanaTexto = ['Domingo', 'Segunda', 'Terça', 'Quarta', 'Quinta', 'Sexta', 'Sabado']
+
+// diaSemanaTexto = diaSemanaTexto[diaSemana]
+
+const data = new Date('2003-12-17');
+const diaSemana = data.getDay();
+
+function getDayText (diaSemana) {
+   let diaSemanaTexto;
+
+   switch (diaSemana) {
+   case 0: // caso diaSemana === 0
+      diaSemanaTexto = 'Domingo';
+      return diaSemanaTexto;
+   case 1:
+      diaSemanaTexto = 'Segunda';
+      return diaSemanaTexto;
+   case 2:
+      diaSemanaTexto = 'Terça';
+      return diaSemanaTexto;
+   case 3:
+      diaSemanaTexto = 'Quarta';
+      return diaSemanaTexto;
+   case 4:
+      diaSemanaTexto = 'Quinta';
+      return diaSemanaTexto;
+   case 5:
+      diaSemanaTexto = 'Sexta';
+      return diaSemanaTexto;
+   case 6:
+      diaSemanaTexto = 'Sabado';
+      return diaSemanaTexto;
+   default:
+      diaSemanaTexto = 'erro';
+   }
+}
+
+const diaSemanaTexto =  getDayText(diaSemana);
+
+console.log(diaSemana, diaSemanaTexto);
+
+// switch (diaSemana) {
+// case 0: // caso diaSemana === 0
+//    diaSemanaTexto = 'Domingo';
+//    break; //finaliza o bloco
+// case 1:
+//    diaSemanaTexto = 'Segunda';
+//    break; //finaliza o bloco
+// case 2:
+//    diaSemanaTexto = 'Terça';
+//    break; //finaliza o bloco
+// case 3:
+//    diaSemanaTexto = 'Quarta';
+//    break; //finaliza o bloco
+// case 4:
+//    diaSemanaTexto = 'Quinta';
+//    break; //finaliza o bloco
+// case 5:
+//    diaSemanaTexto = 'Sexta';
+//    break; //finaliza o bloco
+// case 6:
+//    diaSemanaTexto = 'Sabado';
+//    break; //finaliza o bloco
+// default:
+//    diaSemanaTexto = 'erro';
+// }
+
