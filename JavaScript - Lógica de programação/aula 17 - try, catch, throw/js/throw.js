266ee737b01ede1a591dbc0e5ed99644aa93ff86
vicctorneves <vicctorneves88@gmail.com>
2022-07-12 20:05:10 -0300

    finally

--- JavaScript - Lógica de programação/aula 17 - try, catch, throw/js/throw.js
+++ JavaScript - Lógica de programação/aula 17 - try, catch, throw/js/throw.js
@@ -2,10 +2,15 @@
 
 function soma(x,y){
    if (typeof x !== 'number' || typeof y !== 'number'){
-      throw('x e y precisam ser number')
+      throw new Error('x e y precisam ser number')
    }
    return x + y
 }
+try{
+   console.log(soma(1,'2'));
+} catch (err) {
+   // console.log(err) usado apenas para o back end
+   console.log('erro!')
+}
 
-console.log(soma(1,2));
-console.log(soma(1,'2'));
+console.log('continuando...')
---
+++ JavaScript - Lógica de programação/aula 18 - try, catch, finally/js/script.js
@@ -0,0 +1,10 @@
+try{
+   console.log('1- abrir um arquivo');
+   console.log('2- manipulando o arquivo')
+   console.log(a)
+   console.log('3- fechar o arquivo')
+} catch (error) {
+   console.log('tratando o erro')
+} finally {
+   console.log('sempre sera executado - fechar o arquivo')
+}
