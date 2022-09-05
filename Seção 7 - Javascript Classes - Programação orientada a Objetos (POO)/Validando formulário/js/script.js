229d5680b5fc3c59ba7b581a5460bd816b101bc9
vicctorneves <vicctorneves88@gmail.com>
2022-09-05 20:23:07 -0300

    validação de formulário com class

---
+++ Seção 7 - Javascript Classes - Programação orientada a Objetos (POO)/Validando formulário/js/script.js
@@ -0,0 +1,106 @@
+// Nenhum campo pode estar vazio
+// Usuário só poderá conter letras e/ou números
+// Usuário deverá ter entre 3 e 12 caracteres
+// Senha precisa ter entre 6 e 12 caracteres
+
+const form = document.querySelector('#form')
+const inputs = form.querySelectorAll('form input')
+const inputName = form.querySelector('#input-name');
+const inputLastName = form.querySelector('#input-last-name');
+const inputCPF = form.querySelector('#input-cpf');
+const inputUsuario = form.querySelector('#input-usuario');
+const inputPassword = form.querySelector('#input-password');
+const inputConfirmePassword = form.querySelector('#input-confirme-password');
+const btnRegister = form.querySelector('#button-registrar')
+
+form.addEventListener('submit', function(e){
+   e.preventDefault()
+})
+
+class Valid{
+   constructor(usuario, password, confirmPassword, cpf){
+      this.usuario = usuario.value;
+      this.password = password.value;
+      this.confirmPassword = confirmPassword.value;
+      this.cpf = cpf.value;
+      Object.defineProperty(this, 'cpfLimpo', {
+         configurable: false,
+         enumerable: true,
+         value: this.cpf.replace(/\D+/g, '')
+      })
+   };
+
+   validaUsuario(){
+      isValidOrInvalid(inputUsuario, 3, 12)
+      return this.usuario.length >= 3 || this.usuario.length <= 12
+   };
+   
+   validaSenha(){
+      isValidOrInvalid(inputPassword, 6, 12)
+      return this.password.length >= 6 || this.password.length <= 12
+   };
+   
+   validConfirmSenha(){
+      if(this.confirmPassword === this.password){
+         inputConfirmePassword.classList.remove('invalid')
+      }else{
+         inputConfirmePassword.classList.add('invalid')
+      }
+      return this.confirmPassword !== this.password
+   };
+
+   validCPF(){
+      if (this.cpfLimpo.length !== 11) return false
+      if (this.isSequence()) return false
+      const cpfParcial = this.cpfLimpo.slice(0,-2)
+      const digit1 = Valid.createDigit(cpfParcial)
+      const digit2 = Valid.createDigit(cpfParcial + digit1)
+      const newCpf = cpfParcial + digit1 + digit2
+      return newCpf === this.cpfLimpo
+   };
+
+   static createDigit(cpfParcial){
+      const cpfArray = Array.from(cpfParcial);
+      let regressive = cpfArray.length + 1;
+      const total = cpfArray.reduce((ac, valor)=> {
+        return ac += regressive-- * Number(valor)
+      },0)
+      const digit = 11 - (total % 11)
+      return digit > 9 ? String(0) : String(digit);
+   };
+   
+   isSequence(){
+      return this.cpfLimpo[0].repeat(11) === this.cpfLimpo
+   };
+
+};
+
+function isValidOrInvalid(input, minCarac, maxCarac){
+   if(input.value.length < minCarac || input.value.length > maxCarac){
+      input.classList.remove('invalid');
+   }else{
+      input.classList.add('invalid');
+   }
+}
+
+btnRegister.addEventListener('click', function(){
+   // for (let input of inputs){
+   //    if(input.value === ''){
+   //       alert('Preencha todos os campos')
+   //       return
+   //    }
+   // }
+   const formulario = new Valid(inputUsuario, inputPassword, inputConfirmePassword, inputCPF)
+   formulario.validaUsuario()
+   formulario.validaSenha()
+   if(formulario.validCPF()){
+      inputCPF.classList.remove('invalid')
+   }else{
+      inputCPF.classList.add('invalid')
+   }
+   console.log(formulario.validaUsuario(),
+               formulario.validaSenha(), formulario.validConfirmSenha(), formulario.validCPF())
+
+   if(formulario.validaUsuario() || formulario.validaSenha() || formulario.validConfirmSenha() || formulario.validCPF()) return false
+   alert('Conta Registrada com sucesso')
+})
--- Seção 7 - Javascript Classes - Programação orientada a Objetos (POO)/Validação de cpf com class/script.js
+++ Seção 7 - Javascript Classes - Programação orientada a Objetos (POO)/Validação de cpf com class/script.js
@@ -1,38 +1,55 @@
-// return cpfLimpo = cpf.replace(/\D+/g, '');
+const inputCpf = document.querySelector('#input-cpf');
+const btnValidCPF = document.querySelector('#btn-valid-cpf');
+const p = document.querySelector('#result-cpf')
+
 class ValidaCPF{
    constructor(cpf){
-      this.cpf = cpf;
-      Object.defineProperty(this, 'cpfLimpo', {
-         enumerable: true,
-         value: this.cpf.replace(/\D+/g, '')
-      })
-   }
-   
-   valida(){
-      if(typeof this.cpfLimpo === 'undefined') return false;
-      if(this.cpfLimpo.length !== 11) return false;
-      if(this.isSequence()) return false;
-      const cpfParcial = this.cpfLimpo.slice(0,-2);
-      const digit1 = createDigit(cpfParcial);
-      const digit2 = this.createDigit(cpfParcial + digit1);
-      console.log(digit1, digit2)
+      Object.defineProperty(this, 'cpfLimpo',{
+         enumerable:true,
+         writable:false,
+         configurable: false,
+         value: cpf.replace(/\D+/g, '')
+      });
    }
 
-   createDigit(cpfParcial){
+   valida(){
+      if (this.cpfLimpo.length !== 11) return false
+      if (this.isSequence()) return false
+
+      const cpfParcial = this.cpfLimpo.slice(0,-2)
+      const digit1 = ValidaCPF.createDigite(cpfParcial);
+      const digit2 = ValidaCPF.createDigite(cpfParcial + digit1);
+      const newCpf = cpfParcial + digit1 + digit2
+      return newCpf === this.cpfLimpo
+   }
+
+   static createDigite(cpfParcial){
       const cpfArray = Array.from(cpfParcial);
       let regressive = cpfArray.length + 1;
-      const total = cpfArray.reduce((ac, valor) =>{
-         return ac += regressive-- * Number(valor)
+      const total = cpfArray.reduce((ac,valor)=> {
+        return ac += regressive-- * Number(valor)
       },0)
       const digit = 11 - (total % 11);
       return digit > 9 ? String(0) : String(digit)
    }
 
    isSequence(){
-      const sequencia = this.cpfLimpo[0].repeat(this.cpfLimpo.length);
-      return sequencia === this.cpfLimpo;
+      return this.cpfLimpo[0].repeat(11) === this.cpfLimpo
    }
 }
 
-const cpf = new ValidaCPF('705.484.450-52')
-cpf.valida()
+
+btnValidCPF.addEventListener('click', function(){
+   if (inputCpf.value === '') return
+   const cpf = new ValidaCPF(inputCpf.value);
+   if(cpf.valida()){
+      p.innerText = 'CPF válido';
+      p.classList.remove('cpf-invalid')
+      p.classList.add('cpf-valid')
+   } else{
+      p.innerText = 'CPF inválido'
+      p.classList.remove('cpf-valid')
+      p.classList.add('cpf-invalid')
+   }
+   
+})
