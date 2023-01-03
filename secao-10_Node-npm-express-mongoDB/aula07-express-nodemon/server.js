b35cff9a342dca6e3da00827f9e0802cb99b29da
vicctorneves <vicctorneves88@gmail.com>
2023-01-03 16:18:49 -0300

    ✅feat: Aula06

---
+++ secao-10_Node-npm-express-mongoDB/aula07-express-nodemon/server.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const app = express();
+
+// CRUD -> CREATE, READ, UPDATE, DELETE
+//         POST    GET   PATCH   DELETE
+
+app.get('/', (req, res) => {
+   res.send(`
+      <form action="/" method="POST">
+         Nome: <input type="text" name="nome">
+         <button>Enviar</button>
+      </form>  
+   `)
+});
+
+app.post('/',  (req,res) =>{
+   res.send('Recebi o formulario')
+})
+
+app.get('/contato', (req, res) => {
+   res.send('Contatos')
+});
+
+app.listen(3000, ()=>{
+   console.log('Acessar http://localhost:3000')
+   console.log('Servidor executando na porta  3000')
+})
