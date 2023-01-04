a9f210d534e5d856cec9f50080c61fd7cbebf27e
vicctorneves <vicctorneves88@gmail.com>
2023-01-04 10:18:38 -0300

    Revert "✅aula10 - express views"
    This reverts commit f3cdb65fc69aa224f793d26d8b5f1f538ad0583a.

--- secao-10_Node-npm-express-mongoDB/aula10/src/controllers/homeController.js
+++
@@ -1,8 +0,0 @@
-exports.paginaInicial =(req, res)=>{
-   res.render('index')
-}
-
-exports.trataPost = (req, res) =>{
-   console.log(req.body)
-   res.send(`${req.body.nome} ${req.body.sobrenome}`)
-}
