a2eddf55151ecbc8d075265c284569404b610c36
vicctorneves <vicctorneves88@gmail.com>
2023-01-04 09:23:33 -0300

    ✅aula 8

--- secao-10_Node-npm-express-mongoDB/aula08-req/server.js
+++ secao-10_Node-npm-express-mongoDB/aula08-req/server.js
@@ -1,25 +1,30 @@
 const express = require('express');
 const app = express();
 
-// CRUD -> CREATE, READ, UPDATE, DELETE
-//         POST    GET   PATCH   DELETE
+app.use(express.urlencoded({extended: true}))
 
 app.get('/', (req, res) => {
    res.send(`
       <form action="/" method="POST">
          Nome do cliente: <input type="text" name="nome">
+         Sobrenome: <input type="text" name="sobrenome">
          <button>Enviar</button>
       </form>  
    `)
 });
 
-app.post('/',  (req,res) =>{
-   res.send('Recebi o formulario')
+app.get('/test/:id_user?/:parametro?', (req, res) =>{
+   // req.params = /profiles/3
+   // req.query = /profile/?chave=valor1&chave2=valor2
+   console.log(req.params)
+   console.log(req.query)
+   res.send(req.query.nome + ' ' + req.query.sobrenome)
 })
 
-app.get('/contato', (req, res) => {
-   res.send('Contatos')
-});
+app.post('/',  (req,res) =>{
+   console.log(req.body)
+   res.send(`seu nome é: ${req.body.nome} ${req.body.sobrenome}` )
+})
 
 app.listen(3000, ()=>{
    console.log('Acessar http://localhost:3000')
