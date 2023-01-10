550245d02d0e119644c20c0a53b04717c3d83687
vicctorneves <vicctorneves88@gmail.com>
2023-01-10 09:12:04 -0300

    Revert "Revert "✅feat: Aula03""
    This reverts commit d2d45b7f1500f95c5332974bdfb7d36c2856cff9.

---
+++ secao-10_Node-npm-express-mongoDB/aula16/src/models/HomeModel.js
@@ -0,0 +1,16 @@
+const moongose = require('mongoose');
+
+const HomeSchema = new moongose.Schema({
+   titulo: {type: String, required: true},
+   descricao: String
+});
+
+const HomeModel = moongose.model('Home', HomeSchema);
+
+class Home {
+
+}
+
+
+// module.exports = Home;
+module.exports = HomeModel;
