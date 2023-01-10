d2d45b7f1500f95c5332974bdfb7d36c2856cff9
vicctorneves <vicctorneves88@gmail.com>
2023-01-10 09:11:14 -0300

    Revert "✅feat: Aula03"
    This reverts commit 262e149405549948e09b8a0163f7f75ab8604933.

--- secao-10_Node-npm-express-mongoDB/aula16/src/middlewares/middleware.js
+++
@@ -1,7 +0,0 @@
-exports.middlawareGlobal = (req,res, next) => {
-   res.locals.umaVariavelLocal = 'Este é o valor da variavel local'
-   next();
-}
-exports.outroMiddlaware = (req,res, next) => {
-   next();
-}
